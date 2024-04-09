bc8f1818ad391652ae075e84396978221c52bcd2
liyl <715077252@qq.com>
2024-04-09 13:22:55 +0800

    bst

--- exercises/algorithm/algorithm4.rs
+++ exercises/algorithm/algorithm4.rs
@@ -3,14 +3,13 @@
     This problem requires you to implement a basic interface for a binary tree
 */
 
-//I AM NOT DONE
 use std::cmp::Ordering;
 use std::fmt::Debug;
-#[derive(Clone)]
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct TreeNode<T>
 where
-    T: Ord,T: Clone
+    T: Ord,
+    T: Clone,
 {
     value: T,
     left: Option<Box<TreeNode<T>>>,
@@ -20,14 +19,16 @@ where
 #[derive(Debug)]
 struct BinarySearchTree<T>
 where
-    T: Ord,T: Clone
+    T: Ord,
+    T: Clone,
 {
     root: Option<Box<TreeNode<T>>>,
 }
 
 impl<T> TreeNode<T>
 where
-    T: Ord,T: Clone
+    T: Ord,
+    T: Clone,
 {
     fn new(value: T) -> Self {
         TreeNode {
@@ -40,7 +41,8 @@ where
 
 impl<T> BinarySearchTree<T>
 where
-    T: Ord,T: Clone
+    T: Ord,
+    T: Clone,
 {
     fn new() -> Self {
         BinarySearchTree { root: None }
@@ -48,47 +50,36 @@ where
 
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
-        let mut back = false;
-        match & mut self.root {
+        match &mut self.root {
             None => {
                 self.root = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
             }
             Some(treenode) => {
-                let mut node : Option<Box<TreeNode<T>>>;
-                let mut sign: bool;
-                if treenode.value > value {
-                    node = treenode.left.take();
-                    sign = true;
-                } else {
-                    node = treenode.right.take();
-                    sign = false;
-                }
-                let mut pare = self.root.take();
+                // treenode.insert(value);
+                let mut fuck = treenode; // treenode不能切换引用的对象，声明fuck做这件事
                 loop {
-                    match & mut node {
-                        None => {
-                            if sign {
-                                pare.unwrap().left = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
-                            } else {
-                                pare.unwrap().right = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
+                    if fuck.value == value {
+                        return;
+                    } else if fuck.value > value {
+                        if fuck.left.is_none() {
+                            fuck.left = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
+                            return;
+                        } else {
+                            if let Some(ref mut node) = fuck.left {
+                                fuck = node;
                             }
-                            break;
                         }
-                        Some(child) => {
-                            if sign {
-                                pare = pare.unwrap().left;
+                    } else {
+                        match fuck.right {
+                            None => {
+                                fuck.right =
+                                    Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
+                                return;
                             }
-                            else{
-                                pare = pare.unwrap().right;
+                            Some(ref mut node) => {
+                                fuck = node; //因为这行代码，fuck.right 不能和前面的 
+                                             //self.root 一样使用&mut 而是单独在 Some 中 ref mut
                             }
-                            if child.value > value {
-                                node = child.left.take();
-                                sign = true;
-                            } else {
-                                node = child.right.take();
-                                sign = false;
-                            }
-                            continue;
                         }
                     }
                 }
@@ -99,17 +90,71 @@ where
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        match &self.root {
+            None => {
+                return false;
+            }
+            Some(node) => {
+                return node.search(value);
+            }
+        }
     }
 }
 
 impl<T> TreeNode<T>
 where
-    T: Ord,T: Clone
+    T: Ord,
+    T: Clone,
 {
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
+        if self.value == value {
+            return;
+        } else if self.value > value {
+            match &mut self.left {
+                None => {
+                    self.left = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
+                }
+                Some(lnode) => {
+                    lnode.insert(value);
+                }
+            }
+        } else {
+            match &mut self.right {
+                None => {
+                    self.right = Some(Box::<TreeNode<T>>::new(TreeNode::new(value)));
+                }
+                Some(rnode) => {
+                    rnode.insert(value);
+                }
+            }
+        }
         //TODO
+    }
+    fn search(&self, value: T) -> bool {
+        if self.value == value {
+            return true;
+        } else {
+            if self.value > value {
+                match &self.left {
+                    None => {
+                        return false;
+                    }
+                    Some(lnode) => {
+                        return lnode.search(value);
+                    }
+                }
+            } else {
+                match &self.right {
+                    None => {
+                        return false;
+                    }
+                    Some(rnode) => {
+                        return rnode.search(value);
+                    }
+                }
+            }
+        }
     }
 }
 
